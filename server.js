066e0a1a5e85c2cb1c03980c8a9a57210acd957d
sanskar <sanskargupta37081@gmail.com>
2024-07-02 23:09:10 +0530

    ADDED routes for the different REGISTER,LOGIN,FORGOT PASS,PASS RESET

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config({path : "./config.env"});
 const express = require('express');
 const app = express();
-
+app.use(express.json());
+app.use('/api/auth',require('./routes/auth'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , ()=>
