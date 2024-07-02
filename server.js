049b1c934aa95cec08e7621e804c36311b3a0bae
sanskar <sanskargupta37081@gmail.com>
2024-07-02 23:37:07 +0530

    Connected Mongodb

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require('dotenv').config({path : "./config.env"});
 const express = require('express');
+const connectDB = require('./config/db');
+// Connect DB
+connectDB();
 const app = express();
 app.use(express.json());
 app.use('/api/auth',require('./routes/auth'));
