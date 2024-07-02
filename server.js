136a2ada5274db9efd237dbd3d3bc7209fcf2d2e
sanskar <sanskargupta37081@gmail.com>
2024-07-02 23:55:32 +0530

    ADDED error.js in middleware for errorhandling

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT , ()=>
     console.log('listening on port ${PORT}'));
-// error handling with much more suitable errors
+// error handling with much more suitable syntax
 
 process.on("unhandledRejection", (err, promise) => {
     console.log(`Logged Error: ${err.message}`);
